5d18f485a9c06a5ee0d98d3973f763d8b1bb6b90
Geralmy Urena <geralmyuri11@gmail.com>
2024-05-06 17:26:05 -0700

    new task

--- src/App.js
+++ src/App.js
@@ -3,12 +3,13 @@ import './App.css';
 import { useEffect ,useState} from 'react';
 import Tasklist from './Tasklist';
 import CategoryFilter from './CategoryFilter';
+import NewTaskForm from './NewTaskForm';
 
 
 function App() {
   const [tasks,setTasks] = useState([])
   const [categories,setCategories] = useState(["All", "Code", "Food", "Money", "Misc"])
-  const [selected,setSelected] = useState("All")
+  const [selected,setSelected] = useState("")
 
 
   function handleDeleteTask(id){
@@ -29,7 +30,7 @@ function App() {
     <div className="main-container">
        <CategoryFilter setSelected={setSelected} selected={selected}categories={categories} />
   
-   
+       <NewTaskForm categories={categories}/>
      <Tasklist onDelete={handleDeleteTask} selected={selected} tasks={tasks  }/>
       
     </div>
--- src/Tasklist.js
+++ src/Tasklist.js
@@ -5,14 +5,29 @@ export default function Tasklist({tasks, onDelete,selected}) {
     console.log(tasks)
       let filteredTasks = tasks.filter((task)=> task.category === selected ? task : null)
    
-   
-   
+   // 5.If the button for "All" is selected, all the tasks should be displayed.
+    //maybe if statement for all , else statement for  filter
+    if(selected === "All"){
+      return(
+      <div>
+         {tasks.map((task)=> <Task  selected={selected} onDelete={onDelete} key={task.id} task={task}/>)}
+      </div>
+      )
+      
+
+    }
+
+    else{
+      
       return (
         <div className=''>
           
            {filteredTasks.map(task=><Task  selected={selected} onDelete={onDelete} key={task.id} task={task}/>) }
         </div>
       )
+
+    }
+   
 
     }
    
